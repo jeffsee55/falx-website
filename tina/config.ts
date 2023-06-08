bb0f54a2fa1bae41ec9dd788d0bb36d8bb7ac407
iJrod <26026416+iJrod@users.noreply.github.com>
2023-06-08 16:39:14 +0100

    test

--- tina/config.ts
+++ tina/config.ts
@@ -1,6 +1,4 @@
 import { defineConfig } from "tinacms";
-import { new_pageFields } from "./templates";
-import { postFields } from "./templates";
 
 // Your hosting provider likely exposes this as an environment variable
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
@@ -8,8 +6,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 export default defineConfig({
   branch,
   clientId: "88cf025a-8e30-4dae-ae54-39ff864e3c28", // Get this from tina.io
-  token: "9f5518fe17d83e09ef6379b54016a8d5d9b37ae5", // Get this from tina.io
-  client: { skip: true },
+  token: "cdd8f923c505e4cc09a5e3a17e2133e71e8c1d5d", // Get this from tina.io
+
   build: {
     outputFolder: "admin",
     publicFolder: "static",
@@ -23,294 +21,22 @@ export default defineConfig({
   schema: {
     collections: [
       {
-        format: "md",
-        label: "Homepage (en)",
-        name: "homepage__en_",
-        path: "exampleSite/content/english",
-        frontmatterFormat: "yaml",
-        ui: {
-          allowedActions: {
-            create: false,
-            delete: false,
-          },
-        },
-        match: {
-          include: "_index",
-        },
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
         fields: [
           {
-            type: "rich-text",
-            name: "body",
-            label: "Body of Document",
-            description: "This is the markdown body",
-            isBody: true,
-          },
-        ],
-      },
-      {
-        format: "md",
-        label: "Blog Page (en)",
-        name: "blog_page__en_",
-        path: "exampleSite/content/english/blog",
-        frontmatterFormat: "yaml",
-        ui: {
-          allowedActions: {
-            create: false,
-            delete: false,
-          },
-        },
-        match: {
-          include: "_index",
-        },
-        fields: [
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body of Document",
-            description: "This is the markdown body",
-            isBody: true,
-          },
-        ],
-      },
-      {
-        format: "md",
-        label: "Blog Post (en)",
-        name: "blog_post__en_",
-        path: "exampleSite/content/english/blog",
-        frontmatterFormat: "yaml",
-        match: {
-          include: "**/*",
-          exclude: "_index",
-        },
-        fields: [
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body of Document",
-            description: "This is the markdown body",
-            isBody: true,
-          },
-          ...postFields(),
-        ],
-      },
-      {
-        format: "md",
-        label: "Contact (en)",
-        name: "contact__en_",
-        path: "exampleSite/content/english/contact",
-        frontmatterFormat: "yaml",
-        ui: {
-          allowedActions: {
-            create: false,
-            delete: false,
-          },
-        },
-        match: {
-          include: "_index",
-        },
-        fields: [
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body of Document",
-            description: "This is the markdown body",
-            isBody: true,
-          },
-        ],
-      },
-      {
-        format: "md",
-        label: "Pricing (en)",
-        name: "pricing__en_",
-        path: "exampleSite/content/english/pricing",
-        frontmatterFormat: "yaml",
-        ui: {
-          allowedActions: {
-            create: false,
-            delete: false,
-          },
-        },
-        match: {
-          include: "_index",
-        },
-        fields: [
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body of Document",
-            description: "This is the markdown body",
-            isBody: true,
-          },
-        ],
-      },
-      {
-        format: "md",
-        label: "FAQ (en)",
-        name: "faq__en_",
-        path: "exampleSite/content/english/faq",
-        frontmatterFormat: "yaml",
-        ui: {
-          allowedActions: {
-            create: false,
-            delete: false,
-          },
-        },
-        match: {
-          include: "_index",
-        },
-        fields: [
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body of Document",
-            description: "This is the markdown body",
-            isBody: true,
-          },
-        ],
-      },
-      {
-        format: "md",
-        label: "All Pages",
-        name: "all_pages",
-        path: "exampleSite/content",
-        frontmatterFormat: "yaml",
-        match: {
-          include: "**/*",
-          exclude: "_index",
-        },
-        fields: [
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body of Document",
-            description: "This is the markdown body",
-            isBody: true,
-          },
-          ...new_pageFields(),
-        ],
-      },
-      {
-        format: "toml",
-        label: "Configuration",
-        name: "configuration",
-        path: "exampleSite/config/_default",
-        frontmatterFormat: "yaml",
-        ui: {
-          allowedActions: {
-            create: false,
-            delete: false,
-          },
-        },
-        match: {
-          include: "config",
-        },
-        fields: [
-          {
-            name: "dummy",
-            label: "Dummy field",
             type: "string",
-            description:
-              "This is a dummy field, please replace it with the fields you want to edit. See https://tina.io/docs/schema/ for more info",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
           },
-        ],
-      },
-      {
-        format: "toml",
-        label: "Parameters",
-        name: "parameters",
-        path: "exampleSite/config/_default",
-        frontmatterFormat: "yaml",
-        ui: {
-          allowedActions: {
-            create: false,
-            delete: false,
-          },
-        },
-        match: {
-          include: "params",
-        },
-        fields: [
           {
-            name: "dummy",
-            label: "Dummy field",
-            type: "string",
-            description:
-              "This is a dummy field, please replace it with the fields you want to edit. See https://tina.io/docs/schema/ for more info",
-          },
-        ],
-      },
-      {
-        format: "toml",
-        label: "Variables & Plugins",
-        name: "variables___plugins",
-        path: "/",
-        frontmatterFormat: "yaml",
-        match: {
-          include: "config",
-        },
-        ui: {
-          allowedActions: {
-            create: false,
-            delete: false,
-          },
-        },
-        fields: [
-          {
-            name: "dummy",
-            label: "Dummy field",
-            type: "string",
-            description:
-              "This is a dummy field, please replace it with the fields you want to edit. See https://tina.io/docs/schema/ for more info",
-          },
-        ],
-      },
-      {
-        format: "toml",
-        label: "Languages",
-        name: "languages",
-        path: "exampleSite/config/_default",
-        frontmatterFormat: "yaml",
-        ui: {
-          allowedActions: {
-            create: false,
-            delete: false,
-          },
-        },
-        match: {
-          include: "languages",
-        },
-        fields: [
-          {
-            name: "dummy",
-            label: "Dummy field",
-            type: "string",
-            description:
-              "This is a dummy field, please replace it with the fields you want to edit. See https://tina.io/docs/schema/ for more info",
-          },
-        ],
-      },
-      {
-        format: "toml",
-        label: "Menu (en)",
-        name: "menu__en_",
-        path: "exampleSite/config/_default",
-        frontmatterFormat: "yaml",
-        ui: {
-          allowedActions: {
-            create: false,
-            delete: false,
-          },
-        },
-        match: {
-          include: "menus.en",
-        },
-        fields: [
-          {
-            name: "dummy",
-            label: "Dummy field",
-            type: "string",
-            description:
-              "This is a dummy field, please replace it with the fields you want to edit. See https://tina.io/docs/schema/ for more info",
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
           },
         ],
       },
